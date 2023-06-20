9118d856ca064f96d8440784552c86980c6be79a
leliszx <matheuslelis1010@gmail.com>
2023-06-20 11:19:53 -0300

    mais botões

--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 const play = document.querySelector("#play")
 const aleatorio = document.querySelector("#aleatorio")
+const proximo = document.querySelector("#proximo")
 const musica1 = document.querySelector("#musica1")
 const musica2 = document.querySelector("#musica2")
 const musica3 = document.querySelector("#musica3")
