41d52dbfcede9ac3eeb45d9c38298c24740583af
leliszx <matheuslelis1010@gmail.com>
2023-06-20 10:22:13 -0300

    adicionando js

---
+++ player.js
@@ -0,0 +1,8 @@
+const play = document.querySelector("#play")
+const aleatorio = document.querySelector("#aleatorio")
+const musica1 = document.querySelector("#musica1")
+
+function tocandoEmOrdem(){
+    musica1.play()
+
+}
