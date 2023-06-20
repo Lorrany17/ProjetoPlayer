c3a71d8477584d9c8d7117b4541013dc1593ef42
leliszx <matheuslelis1010@gmail.com>
2023-06-20 10:39:42 -0300

    botão de play funcional

--- player.js
+++ player.js
@@ -1,8 +1,10 @@
 const play = document.querySelector("#play")
 const aleatorio = document.querySelector("#aleatorio")
 const musica1 = document.querySelector("#musica1")
+const musica2 = document.querySelector("#musica2")
+const musica3 = document.querySelector("#musica3")
+const musica4 = document.querySelector("#musica4")
 
 function tocandoEmOrdem(){
     musica1.play()
-
 }
