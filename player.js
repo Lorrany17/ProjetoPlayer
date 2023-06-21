bcb055019cf49e9b6846025ac1b382d8b1fa26c4
Lorrany <santoslorrany250@gmail.com>
2023-06-21 10:29:22 -0300

    Botões de play, voltar e proximo funcionando!

--- player.js
+++ player.js
@@ -6,8 +6,29 @@ const musica1 = document.querySelector("#musica1")
 const musica2 = document.querySelector("#musica2")
 const musica3 = document.querySelector("#musica3")
 const musica4 = document.querySelector("#musica4")
+const musica5 = document.querySelector("#musica5")
+
+const musicas = []
+musicas.push(musica1);
+musicas.push(musica2);
+musicas.push(musica3);
+musicas.push(musica4);
+musicas.push(musica5);
+let indiceAtual = 0
 
 function tocandoEmOrdem(){
-    musica1.play()
+    musicas[indiceAtual].play();
+}
+
+function tocandoProximo(){
+    musicas[indiceAtual].pause()
+    indiceAtual ++
+    musicas[indiceAtual].play()
+}
+
+function tocandoAnterior(){
+    musicas[indiceAtual].pause()
+    indiceAtual --
+    musicas[indiceAtual].play()
 }
 
